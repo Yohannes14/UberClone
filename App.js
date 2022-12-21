51648d3ee6730cfdc577bae10286c0663173d6b4
Yohannes14 <yohansdegu.2010@gmail.com>
2022-12-21 08:40:35 +0300

    uber

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 export default function App() {
   return (
+    <Provider store ={store}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's build Uber!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
