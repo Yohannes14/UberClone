aeb5aa3f2b1bb0e347fd538c587ee9f6dfce4e05
Yohannes14 <yohansdegu.2010@gmail.com>
2022-12-21 10:02:45 +0300

    react navigation

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 
 export default function App() {
+
+  const Stack =createStackNavigator();
   return (
     <Provider store ={store}>
+      <NavigationContainer>
       <SafeAreaProvider>
-     <HomeScreen/>
+        <Stack.Navigator>
+          <Stack.Screen 
+           name ="HomeScreen" component={HomeScreen}
+           options ={{
+            headerShown: false,
+           }}
+          />
+          <Stack.Screen 
+           name ="MapScreen" component={MapScreen}
+           options ={{
+            headerShown: false,
+           }}
+          />
+        </Stack.Navigator>
      </SafeAreaProvider>
+     </NavigationContainer>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Icon } from '@rneui/themed';
 import {FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import tw from "twrnc";
@@ -17,13 +18,15 @@ const data =[
     ];
 
 const NavOptions = () => {
+   const navigation = useNavigation();
   return (
     <FlatList
       data ={data}
       horizontal
       keyExtractor ={(item) => item.id}
       renderItem={({item}) => (
-        <TouchableOpacity style ={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity onPress ={() => navigation.navigate(item.screen)}
+        style ={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
             <View>
                 <Image
                  style ={{
